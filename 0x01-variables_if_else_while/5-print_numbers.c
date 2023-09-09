0c3e008bf17319827a35c5f756b1a8735bff0324
olebogeng <molefeolebogeng16@gmail.com>
2023-09-08 18:58:37 -0700

    print numbers 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-    int number = 0;
+	int number = 0;
 
-    while (number < 10)
-    {
-        putchar(number + '0');
-        number++;
-    }
+	while (number < 10)
+	{
+		putchar(number + '0');
+		number++;
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
