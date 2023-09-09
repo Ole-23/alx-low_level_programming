93be0af522e2eaf7c2e9602dd0587a51d99e67a4
olebogeng <molefeolebogeng16@gmail.com>
2023-09-08 19:07:47 -0700

    print 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints lowercase alphabet in reverse
+ *
+ * Description: Prints the lowercase alphabet in reverse order, followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char letter = 'z';
+
+  while (letter >= 'a')
+    {
+      putchar(letter);
+      letter--;
+    }
+
+  putchar('\n');
+
+  return (0);
+}
