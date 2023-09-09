a748b2308bd72648f83aa3583a29c844c4b1d7d0
olebogeng <molefeolebogeng16@gmail.com>
2023-09-08 18:52:34 -0700

    print numbers 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints single-digit numbers in base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar(number + '0');
+		putchar('\n');
+		number++;
+	}
+
+	return (0);
+}
