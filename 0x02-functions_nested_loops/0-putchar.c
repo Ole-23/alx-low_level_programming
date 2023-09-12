1ea9d0cdddcce697c5ed136ee3016f6b897ff908
olebogeng <molefeolebogeng16@gmail.com>
2023-09-12 16:43:58 -0700

    0x02

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,25 @@
 #include "main.h"
-#include <unistd.h>
-/*** _putchar - writes the character c to stdout*
- *  @c: The character to print** Return: On success
- *   1.* On error, -1 is returned, and errno is set appropriately.
- *   */int _putchar(char c){return (write(1, &c, 1));}=====================================main.h CODE
-#ifndef main_h
-#define main_hint 
-_putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
+#include <stdio.h>
 
+int main(void)
+{
+    FILE *file;
+    int c;
 
+    file = fopen("putchar", "r"); // Open the "putchar" file for reading
 
+    if (file == NULL)
+    {
+        perror("Error opening file");
+        return (1);
+    }
 
+    while ((c = fgetc(file)) != EOF)
+    {
+        _putchar(c); // Print each character from the file
+    }
 
-
+    fclose(file); // Close the file
+    _putchar('\n'); // Print a new line
+    return (0);
+}
