2bf87edccf9637b506f28de7be87033508ba0bae
olebogeng <molefeolebogeng16@gmail.com>
2023-09-12 16:29:52 -0700

    0x02

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,26 @@
 #include "main.h"
+#include <unistd.h>
+/*** _putchar - writes the character c to stdout*
+ *  @c: The character to print** Return: On success
+ *   1.* On error, -1 is returned, and errno is set appropriately.
+ *   */int _putchar(char c){return (write(1, &c, 1));}=====================================main.h CODE
+#ifndef main_h
+#define main_hint 
+_putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 
-/**
-*main - function prints text as output
-*
-*Return:return 0
-*/
-int main(void)
-{
-	char school[10] = "_putchar";
 
-	int i;
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(school[i]);
-	}
-	_putchar(10);
-	return (0);
-}
+
+
+
