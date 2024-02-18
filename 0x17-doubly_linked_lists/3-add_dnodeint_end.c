89eab75a730fcf41342eb4bae828d4bb3a7b141a
olebogeng <molefeolebogeng16@gmail.com>
2024-02-18 05:53:49 -0800

    17

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,36 +1,46 @@
-#include "lists.h"
-#include <stdlib.h>
-
-/**
- * add_dnodeint_end - add element at the end of the list
- * @head: list
- * @n: element in list
- * Return: new element/NULL if failed
+/*
+ * File: 7-insert_dnodeint.c
+ * Auth: Brennan D Baraban
  */
 
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
-{
-	dlistint_t *new, *start;
+#include "lists.h"
 
-	start = *head;
+/**
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp = *h, *new;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	if (*head == NULL)
-	{
-		new->next = NULL;
-		new->n = n;
-		new->prev = NULL;
-		*head = new;
-		return (new);
-	}
-	while (start->next)
-	{
-		start = start->next;
-	}
-	start->next = new;
-	new->prev = start;
-	new->next = NULL;
+
 	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
 	return (new);
 }
